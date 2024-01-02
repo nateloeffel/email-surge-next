89f775881312f7c1a7ea6de504a3c3dda251cdbe
nateloeffel <imrekkrr@gmail.com>
2024-01-01 22:04:21 -0500

    scraping functionality added

---
+++ app/scrape/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Scrape() {
+	return (
+		<div className="m-3 w-full">
+			<h1 className="text-2xl">Scrape</h1>
+		</div>
+	);
+}
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,22 +1,23 @@
 "use server";
 import Link from "next/link";
 
+function NavItem({ href, name }: { href: string; name: string }) {
+	return (
+		<Link
+			href={href}
+			className="text-sm font-medium transition-colors hover:text-primary p-1 w-full bg-red-500 hover:bg-red-800 rounded-md my-2"
+		>
+			<span className="text-lg">{name}</span>
+		</Link>
+	);
+}
+
 export const SideBar = () => {
 	return (
-		<div className="flex flex-col justify-between items-start bg-slate-300 w-1/6">
+		<div className="flex flex-col justify-between items-start bg-slate-300 w-64">
 			<nav className="flex flex-col justify-start items-center p-2 w-full">
-				<Link
-					href="/create"
-					className="text-sm font-medium transition-colors hover:text-primary p-1 w-full bg-red-500 rounded-md my-2"
-				>
-					<span className="text-lg">Create</span>
-				</Link>
-				<Link
-					href="/scrape"
-					className="text-sm font-medium transition-colors hover:text-primary p-1 w-full bg-red-500 rounded-md my-2"
-				>
-					<span className="text-lg">Scrape</span>
-				</Link>
+				<NavItem href="/create" name="Create" />
+				<NavItem href="/scrape" name="Scrape" />
 			</nav>
 			<div className="flex justify-between items-center h-16 bg-slate-500 w-full p-2">
 				{/* Auth */}
